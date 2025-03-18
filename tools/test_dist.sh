74f8171810ed7babc171161379ccbb1b6b9dfa06
Gabriel Siqueira <gabriel.gomes@jemsystems.com>
2025-03-18 14:11:22 -0300

    .gitignore add and updating number of GPUs

--- tools/test_dist.sh
+++ tools/test_dist.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 export PYTHONPATH=./
-GPUS=4
+GPUS=1
 workdir=./inference
 OMP_NUM_THREADS=$GPUS torchrun --nproc_per_node=$GPUS --master_port=$((RANDOM + 10000)) tools/test.py \
 	 $workdir/svg_pointT.yaml  $workdir/best.pth --dist
